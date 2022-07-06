931541bba5121a159317bec00b6eb91e37e68ea3
LauraCPM <cristina2004.ifac@gmail.com>
2022-07-06 07:42:40 -0500

    adicionei site do 1º ano

---
+++ Site_1ano/js/login.js
@@ -0,0 +1,12 @@
+function proximo() 
+{
+	document.getElementById("fotos").src = "logos/eu.jpg";
+}
+function antes()
+{
+	document.getElementById("fotos").src = "logos/daru.jpg";
+}
+function ok()
+{
+	alert("\nLogin concluído com sucesso!");
+}
